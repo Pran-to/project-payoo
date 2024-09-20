77e3b0283a5a087eac99d5cdaeb569378d4a0ada
pran-to <prantochakrobortty24@gmail.com>
2024-09-20 13:16:53 +0600

    set up project payoo

---
+++ js/login.js
@@ -0,0 +1,17 @@
+document.getElementById('btn-login').addEventListener('click',(e)=>{
+
+    e.preventDefault();
+
+   const phoneNumber = document.getElementById('phone-number').value ;
+   const pinNumber =  document.getElementById('pin-number').value ;
+
+   if(phoneNumber === '3' && pinNumber === '123'){
+     
+   }
+   else{
+    alert('worng number or Pin')
+   }
+
+    console.log(phoneNumber ,pinNumber)
+
+})
